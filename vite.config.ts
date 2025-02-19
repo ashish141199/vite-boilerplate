2949994cfd5ef7c29006bc412d8a55226a28f557
Ashish Kulkarni <ashishkulkarni19999@gmail.com>
2025-02-20 02:10:37 +0530

    Made an empty boilerplate;

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '~': '/src'
+    }
+  },
+  server: {
+    port: 3000,
+    allowedHosts: true,
+    host: true,
+  }
 })
